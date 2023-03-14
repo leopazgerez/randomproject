8d59d76b2da9a6c8582468b8fa75c86d37581799
celinaapaza <apazacelinadaniela@gmail.com>
2023-03-14 16:02:04 -0300

    custom button
    agrego componente de boton

--- projecttest/lib/src/ui/pages/home_page.dart
+++ projecttest/lib/src/ui/pages/home_page.dart
@@ -1,11 +1,29 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:projecttest/src/components/custom_button.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: CustomButton(
+          onTap: () {
+            print("siiiiiii al fiiiiin");
+          },
+          color: Colors.green,
+          size: 80,
+          //icon: const Icon(
+          // Icons.favorite,
+          //color: Colors.black,
+          // size: 40,
+          // ),
+          urlImage:
+              "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
+        ),
+      ),
+    );
   }
 }
